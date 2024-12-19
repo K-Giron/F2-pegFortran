7e17a00c029fa975b92f977ef1824cf73799d514
ALEXORR18 <brian7orr@gmail.com>
2024-12-19 11:58:54 -0600

    Plantilla para poder generar metodos para cada produccion visitante

---
+++ fases/fase2/tools/Nodes.js
@@ -0,0 +1,10 @@
+//Plantilla de los nodos a recorrer para poder autogenerarlo
+const nodes = {
+    Producciones: ['id', 'expr', 'alias'],
+    Opciones: ['exprs'],
+    Union: ['exprs'],
+    Expresion: ['expr', 'label', 'qty'],
+    String: ['val', 'isCase'],
+};
+
+export default nodes;
